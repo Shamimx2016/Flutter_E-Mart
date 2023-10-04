ff052b4fabb771679429bac2358c2a9b9a5bc134
shami <shamimbd621@gmail.com>
2023-10-04 15:01:03 +0600

    update

--- lib/view/auth_screen/login_screen.dart
+++ lib/view/auth_screen/login_screen.dart
@@ -1,9 +1,13 @@
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/consts/list.dart';
+import 'package:emart_app/view/auth_screen/signup_screen.dart';
 import 'package:emart_app/widgets_common/applogo_widget.dart';
 import 'package:emart_app/widgets_common/bg_widget.dart';
 import 'package:emart_app/widgets_common/custom_textfield.dart';
 import 'package:emart_app/widgets_common/our_button.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -12,6 +16,7 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return bgWidget(
       child: Scaffold(
+        resizeToAvoidBottomInset: false,
         body: Center(
           child: Column(
             children: [
@@ -19,7 +24,7 @@ class LoginScreen extends StatelessWidget {
               applogoWidgets(),
               10.heightBox,
               "Log in to $appname".text.fontFamily(bold).white.size(18).make(),
-              10.heightBox,
+              15.heightBox,
               Column(
                 children: [
                   customTextField(hint: emailHint, title: email),
@@ -36,7 +41,37 @@ class LoginScreen extends StatelessWidget {
                           onPress: () {})
                       .box
                       .width(context.screenWidth - 50)
-                      .make()
+                      .make(),
+                  5.heightBox,
+                  createNewAccount.text.color(fontGrey).make(),
+                  5.heightBox,
+                  ourButton(
+                      color: lightGold,
+                      title: signup,
+                      textColor: redColor,
+                      onPress: () {
+                        Get.to(() => SignupScreen());
+                      }).box.width(context.screenWidth - 50).make(),
+                  10.heightBox,
+                  loginWith.text.color(fontGrey).make(),
+                  5.heightBox,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      3,
+                      (index) => Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: CircleAvatar(
+                          backgroundColor: lightGrey,
+                          radius: 25,
+                          child: Image.asset(
+                            socialIconList[index],
+                            width: 30,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               )
                   .box
@@ -44,9 +79,8 @@ class LoginScreen extends StatelessWidget {
                   .rounded
                   .padding(EdgeInsets.all(16))
                   .width(context.screenWidth - 70)
+                  .shadowSm
                   .make(),
-              5.heightBox,
-              createNewAccount.text.color(fontGrey).make(),
             ],
           ),
         ),
