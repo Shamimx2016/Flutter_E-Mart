9fbadb98d70e1bd911ff9192b6781ac085c4f6e5
shami <shamimbd621@gmail.com>
2023-10-08 00:36:19 +0600

    update

--- lib/view/categories_screen/item_detiles.dart
+++ lib/view/categories_screen/item_detiles.dart
@@ -1,5 +1,6 @@
 import 'package:emart_app/consts/colors.dart';
 import 'package:emart_app/consts/consts.dart';
+import 'package:emart_app/widgets_common/our_button.dart';
 import 'package:flutter/material.dart';
 
 class ItemDetiles extends StatelessWidget {
@@ -27,7 +28,170 @@ class ItemDetiles extends StatelessWidget {
           ),
         ],
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(8),
+              child: SingleChildScrollView(
+                physics: BouncingScrollPhysics(),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    VxSwiper.builder(
+                      aspectRatio: 16 / 9,
+                      autoPlay: true,
+                      height: 350,
+                      itemCount: 3,
+                      itemBuilder: (context, index) {
+                        return Image.asset(
+                          imgFc5,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
+                    10.heightBox,
+                    title!.text.fontFamily(semibold).size(16).make(),
+                    10.heightBox,
+                    VxRating(
+                      onRatingUpdate: (value) {},
+                      normalColor: textfieldGrey,
+                      selectionColor: golden,
+                      count: 5,
+                      size: 25,
+                      stepInt: true,
+                    ),
+                    10.heightBox,
+                    "\$30,00"
+                        .text
+                        .color(redColor)
+                        .fontFamily(bold)
+                        .size(18)
+                        .make(),
+                    10.heightBox,
+                    Row(
+                      children: [
+                        Expanded(
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              "Seller".text.white.fontFamily(semibold).make(),
+                              5.heightBox,
+                              "In House Brands"
+                                  .text
+                                  .color(darkFontGrey)
+                                  .fontFamily(bold)
+                                  .make(),
+                            ],
+                          ),
+                        ),
+                        const CircleAvatar(
+                          backgroundColor: whiteColor,
+                          child: Icon(
+                            Icons.message_outlined,
+                            color: darkFontGrey,
+                          ),
+                        ),
+                      ],
+                    )
+                        .box
+                        .height(60)
+                        .padding(
+                          EdgeInsets.symmetric(horizontal: 16),
+                        )
+                        .color(textfieldGrey)
+                        .make(),
+                    20.heightBox,
+                    Column(
+                      children: [
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: "Color".text.color(textfieldGrey).make(),
+                            ),
+                            Row(
+                              children: List.generate(
+                                3,
+                                (index) => VxBox()
+                                    .size(40, 40)
+                                    .roundedFull
+                                    .margin(
+                                      EdgeInsets.symmetric(horizontal: 4),
+                                    )
+                                    .color(Vx.randomPrimaryColor)
+                                    .make(),
+                              ),
+                            ),
+                          ],
+                        ).box.padding(EdgeInsets.all(8)).make(),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child:
+                                  "Quantity".text.color(textfieldGrey).make(),
+                            ),
+                            Row(
+                              children: [
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.remove),
+                                ),
+                                "0"
+                                    .text
+                                    .size(16)
+                                    .color(darkFontGrey)
+                                    .fontFamily(bold)
+                                    .make(),
+                                IconButton(
+                                  onPressed: () {},
+                                  icon: Icon(Icons.add),
+                                ),
+                                10.widthBox,
+                                "(0 available)"
+                                    .text
+                                    .color(textfieldGrey)
+                                    .make(),
+                              ],
+                            ),
+                          ],
+                        ).box.padding(EdgeInsets.all(8)).make(),
+                        Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              child: "Total".text.color(textfieldGrey).make(),
+                            ),
+                            "0.00"
+                                .text
+                                .color(redColor)
+                                .size(16)
+                                .fontFamily(bold)
+                                .make(),
+                          ],
+                        ).box.padding(EdgeInsets.all(8)).make(),
+                      ],
+                    ).box.white.roundedSM.make(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ourButton(
+              color: redColor,
+              onPress: () {},
+              textColor: whiteColor,
+              title: "Add to cart",
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
