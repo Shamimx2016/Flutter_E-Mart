50a93c3bd3a41552c4658202fa375c1bd1f21ecf
shami <shamimbd621@gmail.com>
2023-10-14 22:32:07 +0600

    update

---
+++ lib/controller/auth_controller.dart
@@ -0,0 +1,28 @@
+import 'package:emart_app/consts/consts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get/get.dart';
+
+import '../consts/firebase_consts.dart';
+
+class AuthControllers extends GetxController {
+  Future<UserCredential?> loginMethod({email, password, context}) async {
+    UserCredential? userCredential;
+    try {
+      await auth.signInWithEmailAndPassword(email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+    return userCredential;
+  }
+
+  Future<UserCredential?> signupMethod({email, password, context}) async {
+    UserCredential? userCredential;
+    try {
+      await auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+    } on FirebaseAuthException catch (e) {
+      VxToast.show(context, msg: e.toString());
+    }
+    return userCredential;
+  }
+}
